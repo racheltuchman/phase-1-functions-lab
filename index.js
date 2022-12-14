4c51f7bedda041dea12fb9c1928a81646e2357c1
Rachel <rachtuch00@gmail.com>
2022-12-14 10:52:20 -0500

    commit tree 2d79e9537fd1d3108f5e5d2b292a5a9964cf81bc

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+let ft = 264
+let HQ = 42
+
+function distanceFromHqInBlocks (someValue) {
+return Math.abs(someValue - HQ)
+}
+distanceFromHqInBlocks();
+
+function distanceFromHqInFeet(blocks) {
+    let difference = distanceFromHqInBlocks(blocks);
+    return difference * ft
+}
+distanceFromHqInFeet();
+
+
+  
